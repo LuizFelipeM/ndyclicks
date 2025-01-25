bf59774e9a5361d745ea63e1e45c0b6eff7b6c75
Luiz Moura <muniz.mourafelipe@gmail.com>
2025-01-25 13:40:47 -0300

    Carousel for product images

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -6,30 +6,14 @@ import {
   isFilled,
   RichTextField,
 } from "@prismicio/client";
-import React, {
-  createRef,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Simplify } from "../../prismicio-types";
 import { PrismicNextImage } from "@prismicio/next";
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import clsx from "clsx";
 import { RichText } from "./RichText";
 import { JSXMapSerializer } from "@prismicio/react";
-import {
-  CSSTransition,
-  SwitchTransition,
-  TransitionGroup,
-} from "react-transition-group";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import { v4 as uuid } from "uuid";
+import { AnimatePresence, motion } from "framer-motion";
 import "./HeroCarousel.css";
 
 type HeroCarouselProps = {
@@ -45,42 +29,40 @@ const DefaultRichTextComponents: JSXMapSerializer = {
   em: ({ children }) => <i>{children}</i>,
 };
 
-// const HighlightedRichTextComponents: JSXMapSerializer = {
-//   strong: ({ children }) => (
-//     <span className="text-rose text-shadow-md font-serif">
-//       <s>{children}</s>
-//     </span>
-//   ),
-//   paragraph: ({ children }) => (
-//     <span className="text-rose text-shadow-md font-serif">{children}</span>
-//   ),
-//   em: ({ children }) => (
-//     <span className="text-rose text-shadow-md font-serif">
-//       <i>{children}</i>
-//     </span>
-//   ),
-//   heading1: ({ children }) => (
-//     <h1
-//       className="
-//         text-rose text-shadow-md font-serif font-extrabold
-//         tracking-tighter leading-none md:leading-tight
-//         text-[3rem]
-//       "
-//     >
-//       {children}
-//     </h1>
-//   ),
-// };
+interface MappedSlides<T> {
+  key: string;
+  value: T;
+}
 
 export const HeroCarousel: React.FC<HeroCarouselProps> = ({
   beforeHighlightedText,
   afterHighlightedText,
   slides,
-  // ms = 3000,
   ms = 200,
 }) => {
   const [index, setIndex] = useState(0);
-  const nodeRef = useRef<HTMLSpanElement>(null);
+
+  const topTextRef = useRef<HTMLSpanElement>(null);
+
+  const [highlightedTexts, images] = useMemo(
+    () =>
+      slides
+        .filter(
+          (slide) =>
+            isFilled.richText(slide?.highlighted_text) &&
+            isFilled.image(slide?.image)
+        )
+        .reduce<
+          [MappedSlides<RichTextField>[], MappedSlides<ImageField<never>>[]]
+        >(
+          (acc, { highlighted_text, image }) => [
+            [...acc[0], { key: uuid(), value: highlighted_text }],
+            [...acc[1], { key: uuid(), value: image }],
+          ],
+          [[], []]
+        ),
+    [slides]
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -90,102 +72,71 @@ export const HeroCarousel: React.FC<HeroCarouselProps> = ({
   });
 
   return (
-    <div className="max-w-4xl mx-auto relative">
-      <div className="relative w-[600px] h-full overflow-x-hidden flex">
-        {/* absolute */}
-        <div className=" top-4 left-4 text-white">
-          <h1
-            className="
+    <div className="relative h-screen w-full overflow-x-hidden">
+      <div
+        className="
+          absolute inset-0 z-10
+          h-1/3 w-full p-4
+          bg-gradient-to-b from-rich-black to-transparent
+        "
+      >
+        <h1
+          className="
             text-white-smoke font-Roboto font-semibold
               tracking-tighter leading-none md:leading-tight
-              text-[3rem] w-96
+              text-[2.3rem] md:text-[3rem]
+              w-96
             "
-          >
-            <RichText
-              field={beforeHighlightedText}
-              components={DefaultRichTextComponents}
-            />
-
-            <br />
-            <div className="h-16 w-fit overflow-hidden">
-              <SwitchTransition mode="out-in">
-                <CSSTransition
-                  key={index}
-                  nodeRef={nodeRef}
-                  classNames="top-text"
-                  timeout={500}
-                >
-                  <span
-                    ref={nodeRef}
-                    className="block text-rose text-shadow-md font-serif mr-4"
-                  >
-                    <RichText
-                      field={slides[index].highlighted_text}
-                      components={DefaultRichTextComponents}
-                    />
-                  </span>
-                </CSSTransition>
-              </SwitchTransition>{" "}
-            </div>
-
-            <RichText
-              field={afterHighlightedText}
-              components={DefaultRichTextComponents}
-            />
-          </h1>
-        </div>
-
-        {/* {slides.map(
-          ({ image }, i) =>
-            isFilled.image(image) && (
-              <div
-                key={i}
-                style={{ transform: `translateX(-${current * 100}%)` }}
-                className="w-full h-full inline-block flex-none"
-              >
-                <PrismicNextImage
-                  field={image}
-                  className={clsx(
-                    "w-full max-w-[600px] m-auto flex items-center transition-all transform-gpu",
-                    current === i ? "opacity-100" : "opacity-0"
-                  )}
-                />
-              </div>
-            )
-        )} */}
-      </div>
-
-      {/* <div className="absolute inset-0 flex">
-        <div className="flex items-center justify-start w-1/2">
-          <button
-            className="bg-rich-black hover:text-rose shadow-white-smoke hover:shadow-lg rounded-full w-12 h-12 -ml-6"
-            onClick={previous}
-          >
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </button>
-        </div>
-        <div className="flex items-center justify-end w-1/2">
-          <button
-            className="bg-rich-black hover:text-rose hover:shadow rounded-full w-12 h-12 -mr-6"
-            onClick={next}
-          >
-            <FontAwesomeIcon icon={faChevronRight} />
-          </button>
-        </div>
-      </div>
-
-      <div className="absolute w-full flex items-center justify-center px-4">
-        {slides.map((_, i) => (
-          <button
-            key={i}
-            className={clsx(
-              "flex-1 w-4 h-2 mt-4 mx-2 mb-0 rounded-full overflow-hidden transition-colors duration-200 ease-out hover:bg-savoy-blue hover:shadow-lg cursor-pointer",
-              current === i ? "bg-white-smoke" : "bg-dim-gray"
-            )}
-            onClick={() => setCurrent(i)}
+        >
+          <RichText
+            field={beforeHighlightedText}
+            components={DefaultRichTextComponents}
           />
-        ))}
-      </div> */}
+
+          <br />
+          <div className="h-[2.6rem] md:h-16 w-fit overflow-hidden">
+            <SwitchTransition mode="out-in">
+              <CSSTransition
+                key={highlightedTexts[index].key}
+                nodeRef={topTextRef}
+                classNames="top-text"
+                timeout={350}
+              >
+                <span
+                  ref={topTextRef}
+                  className="block text-rose text-shadow-md font-serif mr-4"
+                >
+                  <RichText
+                    field={highlightedTexts[index].value}
+                    components={DefaultRichTextComponents}
+                  />
+                </span>
+              </CSSTransition>
+            </SwitchTransition>{" "}
+          </div>
+
+          <RichText
+            field={afterHighlightedText}
+            components={DefaultRichTextComponents}
+          />
+        </h1>
+      </div>
+
+      <AnimatePresence>
+        <motion.div
+          key={images[index].key}
+          className="absolute inset-0 w-full h-full max-h-svh overflow-hidden"
+          initial={{ x: "100%" }}
+          animate={{ x: "0%" }}
+          exit={{ x: "-100%" }}
+          transition={{ duration: 0.35 }}
+        >
+          <PrismicNextImage
+            field={images[index].value}
+            className="object-cover w-full h-full absolute top-0 left-0"
+          />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- src/slices/Hero/HeroProducts.tsx
+++ src/slices/Hero/HeroProducts.tsx
@@ -25,7 +25,7 @@ export const HeroProducts: React.FC<HeroProductsProps> = ({
   carousel,
 }) => {
   return (
-    <div className="relative bg-gray-100 p-6 rounded-lg shadow-lg">
+    <div className="relative bg-gray-100">
       {isFilled.group(carousel) && (
         <HeroCarousel
           beforeHighlightedText={beforeHighlightedText}
@@ -34,18 +34,20 @@ export const HeroProducts: React.FC<HeroProductsProps> = ({
           ms={5000}
         />
       )}
-      <div className="absolute bottom-4 left-4">
+      <div className="absolute bottom-24 left-1/2 -translate-x-1/2 z-10">
         {buttons?.map(
           ({ text, link }, i) =>
             isFilled.keyText(text) &&
             isFilled.link(link) && (
-              <LinkButton key={i} href={link} className="font-bold">
+              <LinkButton key={i} href={link} className="font-bold shadow-md">
                 {text}
               </LinkButton>
             )
         )}
       </div>
-      <div className="absolute bottom-4 right-4 text-blue-500"> Nandayara </div>
+      <span className="absolute bottom-4 right-4 text-savoy-blue text-3xl font-bold font-serif text-shadow-md z-10">
+        Nandayara
+      </span>
     </div>
   );
 };

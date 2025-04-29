91fe3729194cdf5826d3a2c81ce3fadce41f643a
Luiz Moura <muniz.mourafelipe@gmail.com>
2025-04-29 15:02:47 -0300

    Revert changes

--- src/components/ImageFullScreen/index.tsx
+++ src/components/ImageFullScreen/index.tsx
@@ -16,13 +16,15 @@ export const ImageFullScreen: React.FC<ImageFullScreenProps> = ({
     <div className="relative max-w-6xl w-[80vw] md:w-screen h-[50vh] mx-auto items-center overflow-hidden aspect-[6/7] rounded-tl-md rounded-br-md rounded-tr-[80px] rounded-bl-[80px]">
       <PrismicNextImage
         field={image}
-        className={clsx(
-          "object-cover absolute h-full w-full inset-0 mt-0",
-          {
-            [`md:h-[unset] md:mt-[-${bottom}px]`]:
-              isFilled.number(bottom),
-          }
-        )}
+        fill
+        className="object-cover"
+        // className={clsx(
+        //   "object-cover absolute h-full w-full inset-0 mt-0",
+        //   {
+        //     [`md:h-[unset] md:mt-[-${bottom}px]`]:
+        //       isFilled.number(bottom),
+        //   }
+        // )}
       />
     </div>
   );
